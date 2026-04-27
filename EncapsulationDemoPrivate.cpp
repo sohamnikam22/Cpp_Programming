dc181272e877bd2534cd0aa06310cd7e19c3b57a
sohamnikam22 <sohamnikam2202@gmail.com>
2026-04-27 22:05:28 +0530

    Add files via upload

---
+++ EncapsulationDemoPrivate.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+// Encapsulation
+class Marvellous
+{
+    // Access specifier (By Default private)
+    
+    int No1, No2;   // Characsteristics
+
+    void Fun()      // Beehaviour
+    {
+        cout<<"Inside Fun\n";   
+    }
+
+    void Gun()       // Behaviour
+    {
+        cout<<"Inside Gun\n"; 
+    }
+
+
+};
+
+int main()
+{
+    // Object creation (Instance)
+    Marvellous mobj1;
+    Marvellous mobj2;
+
+
+    cout<<sizeof(mobj1)<<"\n";  // 8
+
+
+    cout<<mobj1.No1<<"\n";
+
+    mobj1.Fun();
+    mobj2.Fun();
+    
+    mobj1.Gun();
+
+    return 0;
+}
+// This will generate error
