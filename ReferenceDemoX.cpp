500d41a1c33782d88c546be9d5dfbaac18b0f53f
sohamnikam22 <sohamnikam2202@gmail.com>
2026-05-08 22:30:35 +0530

    Add files via upload

---
+++ ReferenceDemoX.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   
+   // We can create one reference to one variable
+   int no = 11;
+   int &x = no;
+   
+   // We can create multiple refrences for single variable
+   int &y = no;
+
+   // We can create reference to reference
+   int &z = y;
+
+   // We can create reference to pointer
+   int *p = &no;
+   int *(&q) = p;
+
+
+
+
+   return 0;
+}
